570f6d33f47ff5f86c1dcfcf32d6056a4176914a
Nelly <Nelly.sosobrado@gmail.com>
2025-03-11 11:23:13 +0100

    added contact us now. Also responds to resolution, only displays on mobile mode

--- src/components/sections/FAQ.jsx
+++ src/components/sections/FAQ.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Accordion from '../elements/Accordion'
 import ContactOption from '../elements/ContactOption'
+import LinkButton from '../elements/LinkButton'
 
 const Faq = () => {
   return (
@@ -16,6 +17,15 @@ const Faq = () => {
             <div className="contact-options">
                 <ContactOption icon="fa-solid fa-phone" text="Still have questions?" uri="/contact" linkText="Contact us" color="purple" />
                 <ContactOption icon="bi bi-chat-dots-fill" text="Don't like phone calls?" uri="/contact" linkText="Contact us" color="green" />
+            </div>
+
+            <div className="mobile-only">
+                <LinkButton 
+                    to="/contact"
+                    color="purple"
+                    text="Contact Us Now"
+                    icon="fa-regular fa-envelope"
+                />
             </div>
 
             <Accordion />
